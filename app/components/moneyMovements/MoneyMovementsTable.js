7a46ac646764a382f79933364266ffa1efe7282e
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2019-12-10 23:25:07 +0000

    Show account instead of user in mm table

--- app/components/moneyMovements/MoneyMovementsTable.js
+++ app/components/moneyMovements/MoneyMovementsTable.js
@@ -8,6 +8,7 @@ import { Icon } from 'components/ui/Icon';
 import { SidePanel } from 'components/ui/SidePanel';
 import { Table } from 'components/ui/table/Table';
 
+import { ACCOUNTS_BASE_URL } from '../accounts/constants';
 import { CATEGORIES_BASE_URL } from '../categories/constants';
 import { MONEY_MOVEMENTS_BASE_URL } from '../moneyMovements/constants';
 import { MoneyMovementDetail } from './MoneyMovementDetail';
@@ -19,7 +20,7 @@ export function MoneyMovementsTable({ moneyMovements, finance }) {
     const numberStyle = { textAlign: 'right', paddingRight: '10px' };
     const columns = [
         { prop: 'actions', title: '', width: 50, padding: 0 },
-        { prop: 'user', title: '', width: 50, padding: 0 },
+        { prop: 'account', title: '', width: 50, padding: 0 },
         { prop: 'movement_icon', title: '', width: 30, padding: 0 },
         { prop: 'amount', title: 'Amount', width: 100, style: numberStyle },
         { prop: 'movement_date', title: 'Date', width: 120 },
@@ -34,7 +35,9 @@ export function MoneyMovementsTable({ moneyMovements, finance }) {
         .sort((mm1, mm2) => mm1.movement_date > mm2.movement_date ? -1 : 1)
         .map(mm => ({
             ...mm,
-            user: <Badge type="circular">{finance.users[mm.user].initials}</Badge>,
+            account: mm.account && <ALink to={`${ACCOUNTS_BASE_URL}/${mm.account}`} classes={['borderless']}>
+                <Badge type="circular">{finance.accounts[mm.account].short_name}</Badge>
+            </ALink>,
             category: <ALink to={`${CATEGORIES_BASE_URL}/${mm.category}`}>{finance.categories[mm.category].full_name}</ALink>,
             movement_icon: mm.movement === '-' ? <Icon name="arrow_downward" className="red" size="small" /> : <Icon name="arrow_upward" className="teal" size="small" />,
             master_total: parseFloat(mm.master_total) ? mm.master_total : '',
